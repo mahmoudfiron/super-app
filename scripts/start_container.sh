b1f3366b2cf6ec213e4a674270d85b6880a512ec
mahmoudfiron <mahmoudfiron13@gmail.com>
2025-08-11 01:50:00 +0300

    fix start

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -7,10 +7,18 @@ docker pull mahmoudfiron/super-app-php:latest
 docker pull mysql:8.0.28
 
 # Start MySQL first
-docker run -d --name super-app-mysql \ -p 3306:3306 \ -e MYSQL_DATABASE=super-app \ -e MYSQL_ROOT_PASSWORD=password \ mysql:8.0.28
+docker run -d --name super-app-mysql \
+  -p 3306:3306 \
+  -e MYSQL_DATABASE=super-app \
+  -e MYSQL_ROOT_PASSWORD=password \
+  mysql:8.0.28
 
 # PHP app listens on 80 in the container → expose as 8080 outside
-docker run -d --name super-app-php \ -p 8080:80 \ mahmoudfiron/super-app-php:latest
+docker run -d --name super-app-php \
+  -p 8080:80 \
+  mahmoudfiron/super-app-php:latest
 
 # Node app listens on 3000 in the container → expose as 3000 outside
-docker run -d --name super-app-node \ -p 3000:3000 \ mahmoudfiron/super-app-node:latest
+docker run -d --name super-app-node \
+  -p 3000:3000 \
+  mahmoudfiron/super-app-node:latest
